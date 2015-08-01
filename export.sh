e0a158d549119747db17d8203e93b0b01821c926
Kevin Caccamo <kevin@ciinet.org>
2015-08-01 02:04:46 -0400

    Add shell/bash script to copy python scripts to Blender scripts folder

---
+++ export.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Export the scripts to the Blender scripts folder
+
+homedir=`echo $HOME | sed 's/\ /\\\ /g'`
+
+if [ ! -z $(uname | grep -E 'MINGW|Cygwin') ]; then
+  # We're using Bash on Windows!
+  homedir="$homedir/Documents/"
+fi
+
+if [ -e import_iff.py ]; then
+blender_scripts_folder="$homedir/BlenderScriptsDev/addons/io_scene_wcp" # Development script folder
+cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh,import_iff}.py
+else
+blender_scripts_folder="$homedir/BlenderScripts/addons/io_scene_wcp" # Production script folder
+cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh}.py
+fi
+
+if [ -d $blender_scripts_folder/__pycache__ ]; then
+  rm -r $blender_scripts_folder/__pycache__
+fi
