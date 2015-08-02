ebd2bacc8a2b882972d19d4a3b33e9922966b6f9
Kevin Caccamo <kevin@ciinet.org>
2015-08-02 00:51:14 -0400

    Fix bash script
    Fix bash script not working with directory names with spaces

--- export.sh
+++ export.sh
@@ -11,10 +11,10 @@ fi
 
 # if [ -e import_iff.py ]; then
 # blender_scripts_folder="$homedir/BlenderScriptsDev/addons/io_scene_wcp" # Development script folder
-# cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh,import_iff}.py
+# eval cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh,import_iff}.py
 # else
 blender_scripts_folder="$homedir/BlenderScripts/addons/io_scene_wcp" # Production script folder
-cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh}.py
+eval cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh}.py
 # fi
 
 if [ -d $blender_scripts_folder/__pycache__ ]; then
