57d9cea7e27dc000d09ba2ea6cfff43157ae129a
Kevin Caccamo <kevin@ciinet.org>
2015-08-02 00:22:19 -0400

    Modify export.sh to copy all files to production folder

--- export.sh
+++ export.sh
@@ -6,16 +6,16 @@ homedir=`echo $HOME | sed 's/\ /\\\ /g'`
 
 if [ ! -z $(uname | grep -E 'MINGW|Cygwin') ]; then
   # We're using Bash on Windows!
-  homedir="$homedir/Documents/"
+  homedir="$homedir/Documents"
 fi
 
-if [ -e import_iff.py ]; then
-blender_scripts_folder="$homedir/BlenderScriptsDev/addons/io_scene_wcp" # Development script folder
-cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh,import_iff}.py
-else
+# if [ -e import_iff.py ]; then
+# blender_scripts_folder="$homedir/BlenderScriptsDev/addons/io_scene_wcp" # Development script folder
+# cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh,import_iff}.py
+# else
 blender_scripts_folder="$homedir/BlenderScripts/addons/io_scene_wcp" # Production script folder
 cp --target-directory=$blender_scripts_folder {__init__,export_iff,iff,iff_mesh}.py
-fi
+# fi
 
 if [ -d $blender_scripts_folder/__pycache__ ]; then
   rm -r $blender_scripts_folder/__pycache__
