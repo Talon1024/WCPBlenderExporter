Fix D-Plane calculation.
The D-Plane is calculated from the coordinates of the first vertex on the face, and the face normal, not the vertex normal.

export_iff.py
@@ -628,7 +628,7 @@ class ModelManager:
                     # Add the face
                     ilodm.add_face(
                         unique_normals[fnrm],
-                        self.calc_dplane(first_vert.co, first_vert.normal),
+                        self.calc_dplane(first_vert.co, tf.normal),
                         texnum, fvrt_idx, len(tf.vertices), light_flags)
                     fvrt_idx += len(tf.vertices)
             else: