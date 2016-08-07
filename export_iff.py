d0cd69e21cdf47248c5f8ea9a10a7c43ecc0dbf5
Kevin Caccamo <kevin@ciinet.org>
2016-08-07 01:14:20 -0400

    Fix Y rotation component.

--- export_iff.py
+++ export_iff.py
@@ -336,6 +336,7 @@ class ModelManager:
                         hp_euler.rotate(self.wc_matrix)
                         hp_euler.rotate(self.HP_WC_XFM)
                         hp_euler.x *= -1
+                        hp_euler.y, hp_euler.z = hp_euler.z, -hp_euler.y
                         hp_matrix = hp_euler.to_matrix()
                         hp_loc = cobj.location.copy()
                         hp_loc.rotate(self.wc_matrix)
