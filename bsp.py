0d18fc094e27178ebd20876ff2b117ec51e613f0
Kevin Caccamo <kevin@ciinet.org>
2016-11-17 18:40:51 -0500

    Initial work on BSP tree builder for Wing Blender.
    Add BSPNode and BSPTree classes.
    Add code to convert BSP trees and nodes to IFF format.

---
+++ bsp.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+from . import iff
+
+
+class BSPNode:
+
+    def __init__(self, a, b, c, d, back=None, front=None, boundary=None):
+        # I think VISION's BSP system is based on the plane equation
+        # Ax + By + Cz + D = 0
+        self.a = float(a)
+        self.b = float(b)
+        self.c = float(c)
+        self.d = float(d)
+
+        self.back = back  # Node behind
+        self.front = front  # Node in front
+        self.boundary = boundary  # Vertices of boundary plane(?)
+
+    def to_iff(self):
+        data_chunk = iff.IffChunk(
+            # I used 0 and -1 as the first and last members because that's what
+            # I noticed when I looked at the DATA chunks in models from the
+            # original game.
+            "DATA", 0, self.a, self.b, self.c, self.d, -1)
+        back_form = iff.IffForm("BACK", self.back)
+        front_form = iff.IffForm("FRNT", self.front)
+        vert_chunk = iff.IffChunk("VERT", self.boundary)
+
+        return (data_chunk, back_form, front_form, vert_chunk)
+
+
+class BSPTree:
+
+    def __init__(self, root_node):
+        self.root_node = root_node
+
+    def to_iff(self):
+        tree_form = iff.IffForm("TREE", root_node.to_iff())
+        return tree_form
